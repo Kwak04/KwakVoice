21e45492aca7aad4e45adf6c22be2151ad7d3193
Kwak HyunMin <48079406+Kwak04@users.noreply.github.com>
2019-07-18 00:40:05 +0900

    ㅇㅡㅇ

--- KwakVoice.py
+++ KwakVoice.py
@@ -1,27 +1,65 @@
 import speech_recognition as sr
+import pyttsx3
+
 import webbrowser
 import os
 import time
-import pyttsx3
+
 
 r = sr.Recognizer()
 engine = pyttsx3.init()
 
+
+def do(speaker_text, text_to_print, text_to_say, web_url):
+    if web_url == 0:
+        if text == speaker_text:
+            print(text_to_print)
+            if text_to_say == 0:
+                engine.say(text_to_print)
+            else:
+                engine.say(text_to_say)
+            engine.runAndWait()
+    else:
+        if text == speaker_text:
+            print(text_to_print)
+            if text_to_say == 0:
+                engine.say(text_to_print)
+            else:
+                engine.say(text_to_say)
+
+            engine.runAndWait()
+            webbrowser.open(web_url)
+
+
 with sr.Microphone() as source:
-    while True:
-        print("What can I help you with?")
+
+    for i in range(1):
+        print("무엇을 도와드릴까요?\n>>>", end=" ")
         audio = r.listen(source)
 
         try:
-            text = r.recognize_google(audio)
+            text = r.recognize_google(audio, language="ko-KR")
+            print(text)
 
-            if text == "hello":
-                print(text)
-                print("OK")
-                engine.say("안녕하세요.")
-                engine.runAndWait()
+            # 명령들
+            do("유튜브 열어 줘", "유튜브 여는 중...", "알겠습니다. 유튜브 여는 중.", "https://www.youtube.com")
+            do("안녕", "안녕하세요!", 0, 0)
+            do("안녕하세요", "안녕하세요", "안녕하십니까!", 0)
+            
 
         except:
-            print("오류입니다.")
+            print("\n인식에 실패하였습니다. 다시 시도해 주세요.")
+            engine.say("인식에 실패하였습니다. 다시 시도해 주세요.")
+            engine.runAndWait()
 
-            engine.say("오류입니다.")
+
+
+
+
+
+
+
+
+
+
+
