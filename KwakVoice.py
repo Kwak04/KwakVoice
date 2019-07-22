b5787c595b099a531d6e0a8204f7515f0829ee6d
Kwak HyunMin <48079406+Kwak04@users.noreply.github.com>
2019-07-22 23:07:14 +0900

    함수 인수 도움말 생성

--- KwakVoice.py
+++ KwakVoice.py
@@ -9,6 +9,13 @@ engine = pyttsx3.init()
 
 
 def do(speaker_text, text_to_print, text_to_say, speaker_text_to_print, web_url):
+    # 함수 인수 도움말
+    # speaker_text: 마이크로 인식되는 사용자의 텍스트
+    # text_to_print: KwakVoice가 콘솔에 출력할 메시지
+    # text_to_say: KwakVoice가 tts로 말할 메시지 (text_to_print와 같은 경우 0 입력)
+    # speaker_text_to_print: 사용자의 텍스트를 인식되는 것과 다르게 표시하고 싶은 경우 텍스트 입력 (speaker_text와 같으면 0 입력)
+    # web_url: 웹사이트를 표시하고 싶은 경우 url 입력 (웹사이트를 표시하지 않아도 되는 경우 0 입력)
+
     if web_url == 0:
         if text == speaker_text:
             if speaker_text_to_print == 0:
@@ -52,7 +59,7 @@ with sr.Microphone() as source:
             # # 대화
             do("안녕", "안녕하세요!", 0, 0, 0)
             do("안녕하세요", "안녕하세요", "안녕하십니까!", 0, 0)
-            
+
             # # 웹사이트 열기
             do("유튜브 열어 줘", "유튜브 여는 중...", "알겠습니다. 유튜브 여는 중.", "유튜브 열어줘", "https://www.youtube.com")
 
